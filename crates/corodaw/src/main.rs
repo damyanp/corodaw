0f6e3ccc989ee5b51e0d448528bd23441c98a995
Damyan Pepper <damyanp@gmail.com>
2025-12-21 21:28:06 -0800

    Some interactivity

--- crates/corodaw/src/main.rs
+++ crates/corodaw/src/main.rs
@@ -1,22 +1,74 @@
 use gpui::*;
-use gpui_component::{button::*, *};
+use gpui_component::{
+    button::*,
+    slider::{Slider, SliderState},
+    *,
+};
 
-pub struct Example;
-impl Render for Example {
-    fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
+struct Module {
+    name: String,
+    main_volume: Entity<SliderState>,
+}
+
+impl Module {
+    pub fn new(cx: &mut Context<Self>, name: String) -> Self {
+        let main_volume = cx.new(|_| SliderState::new().min(0.0).max(1.0));
+
+        Self { name, main_volume }
+    }
+}
+
+impl Render for Module {
+    fn render(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .border_1()
+            .border_color(cx.theme().border)
+            .p_5()
+            .child(
+                h_flex()
+                    .gap_2()
+                    .child(self.name.clone())
+                    .child(Slider::new(&self.main_volume).min_w_128()),
+            )
+    }
+}
+
+pub struct Corodaw {
+    modules: Vec<Entity<Module>>,
+    counter: u32,
+}
+
+impl Corodaw {
+    fn new(cx: &mut Context<Self>) -> Self {
+        Self {
+            modules: vec![cx.new(|cx| Module::new(cx, "Master".to_owned()))],
+            counter: 0,
+        }
+    }
+
+    fn on_click(&mut self, _: &ClickEvent, _: &mut Window, cx: &mut Context<Self>) {
+        self.counter += 1;
+        self.modules
+            .push(cx.new(|cx| Module::new(cx, format!("Module {}", self.counter))));
+    }
+}
+
+impl Render for Corodaw {
+    fn render(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         div()
             .v_flex()
             .gap_2()
             .size_full()
-            .items_center()
-            .justify_center()
-            .child("Hello, World!")
+            .items_start()
+            .justify_start()
+            .p_10()
             .child(
                 Button::new("ok")
                     .primary()
-                    .label("Let's Go!")
-                    .on_click(|_, _, _| println!("Clicked!")),
+                    .label("Add Module")
+                    .on_click(cx.listener(Self::on_click)),
             )
+            .children(self.modules.iter().map(|m| div().w_full().child(m.clone())))
     }
 }
 
@@ -29,7 +81,7 @@ fn main() {
 
         cx.spawn(async move |cx| {
             cx.open_window(WindowOptions::default(), |window, cx| {
-                let view = cx.new(|_| Example);
+                let view = cx.new(|cx| Corodaw::new(cx));
                 // This first level on the window, should be a Root.
                 cx.new(|cx| Root::new(view, window, cx))
             })?;
