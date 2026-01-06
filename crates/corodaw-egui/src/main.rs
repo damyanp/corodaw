728254d6744bd2ed2cca022ed3be11323318ddbe
Damyan Pepper <damyanp@gmail.com>
2026-01-05 20:32:50 -0800

    Refactor Corodaw to remove self-referencing Rc and Weak
    Simplify Corodaw struct by eliminating Rc<RefCell<Self>> and Weak.
    Refactor module management to use Rc<RefCell<Vec<Module>>>. Update
    add_module to operate directly on self and spawn tasks without
    self-upgrade. Adjust main and App to use Corodaw directly.

--- crates/corodaw-egui/src/main.rs
+++ crates/corodaw-egui/src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    cell::RefCell,
-    rc::{Rc, Weak},
-};
+use std::{cell::RefCell, rc::Rc};
 
 use eframe::egui::{self, ComboBox};
 use engine::{
@@ -20,8 +17,6 @@ mod module;
 mod plugins;
 
 struct Corodaw<'a> {
-    this: Weak<RefCell<Self>>,
-
     executor: Rc<LocalExecutor<'a>>,
 
     found_plugins: Vec<Rc<FoundPlugin>>,
@@ -29,7 +24,7 @@ struct Corodaw<'a> {
 
     selected_plugin: Option<Rc<FoundPlugin>>,
 
-    modules: Vec<Module>,
+    modules: Rc<RefCell<Vec<Module>>>,
     counter: u32,
 
     audio_graph: Rc<RefCell<AudioGraph>>,
@@ -37,26 +32,21 @@ struct Corodaw<'a> {
 }
 
 impl<'a> Corodaw<'a> {
-    fn new(executor: Rc<LocalExecutor<'a>>) -> Rc<RefCell<Self>> {
+    fn new(executor: Rc<LocalExecutor<'a>>) -> Self {
         let manager = EguiClapPluginManager::new(&executor);
         let (audio_graph, audio_graph_worker) = audio_graph();
         let audio = Audio::new(audio_graph_worker).unwrap();
 
-        let r = Rc::new(RefCell::new(Self {
-            this: Weak::default(),
+        Self {
             executor,
             found_plugins: get_plugins(),
             manager,
             selected_plugin: None,
-            modules: Vec::default(),
+            modules: Rc::default(),
             counter: 0,
             audio_graph: Rc::new(RefCell::new(audio_graph)),
             _audio: audio,
-        }));
-
-        r.borrow_mut().this = Rc::downgrade(&r);
-
-        r
+        }
     }
 
     fn update(&mut self, ctx: &egui::Context) {
@@ -64,12 +54,7 @@ impl<'a> Corodaw<'a> {
             ui.horizontal(|ui| {
                 ui.add_enabled_ui(self.selected_plugin.is_some(), |ui| {
                     if ui.button("Add Module").clicked() {
-                        let clone = self.this.upgrade().unwrap();
-                        self.executor
-                            .spawn(async move {
-                                Corodaw::add_module(&clone).await;
-                            })
-                            .detach();
+                        self.add_module();
                     }
                 });
                 ComboBox::from_id_salt("Plugin")
@@ -85,7 +70,7 @@ impl<'a> Corodaw<'a> {
                         }
                     });
             });
-            for module in &self.modules {
+            for module in self.modules.borrow().iter() {
                 module.add_to_ui(self, ui);
             }
         });
@@ -105,22 +90,21 @@ impl<'a> Corodaw<'a> {
         self.manager.has_plugin_gui(plugin)
     }
 
-    async fn add_module(this: &Rc<RefCell<Self>>) {
-        let module = {
-            let mut s = this.borrow_mut();
-            let manager = s.manager.inner.clone();
+    fn add_module(&mut self) {
+        let plugin = self.selected_plugin.as_ref().unwrap().clone();
+        let name = format!("Module {}: {}", self.counter, plugin.name);
+        self.counter += 1;
 
-            let plugin = s.selected_plugin.as_ref().unwrap().clone();
-            let name = format!("Module {}: {}", s.counter, plugin.name);
-            s.counter += 1;
+        let modules = self.modules.clone();
+        let audio_graph = self.audio_graph.clone();
+        let manager = self.manager.inner.clone();
 
-            let audio_graph = s.audio_graph.clone();
-
-            Module::new(name, plugin, manager, audio_graph)
-        }
-        .await;
-
-        this.borrow_mut().modules.push(module);
+        self.executor
+            .spawn(async move {
+                let module = Module::new(name, plugin, manager, audio_graph).await;
+                modules.borrow_mut().push(module);
+            })
+            .detach();
     }
 }
 
@@ -135,24 +119,26 @@ fn main() -> eframe::Result {
     let options = eframe::NativeOptions::default();
 
     struct App<'a> {
-        corodaw: Rc<RefCell<Corodaw<'a>>>,
+        corodaw: Corodaw<'a>,
         executor: Rc<LocalExecutor<'a>>,
     }
     impl eframe::App for App<'_> {
         fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
             while self.executor.try_tick() {}
 
-            self.corodaw.borrow_mut().update(ctx);
+            self.corodaw.update(ctx);
         }
     }
-
-    let executor = Rc::new(LocalExecutor::new());
-    let corodaw = Corodaw::new(executor.clone());
 
     eframe::run_native(
         "Corodaw",
         options,
-        Box::new(|_| Ok(Box::new(App { executor, corodaw }))),
+        Box::new(|_| {
+            let executor = Rc::new(LocalExecutor::new());
+            let corodaw = Corodaw::new(executor.clone());
+
+            Ok(Box::new(App { executor, corodaw }))
+        }),
     )
     .unwrap();
 
