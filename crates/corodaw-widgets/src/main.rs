945b7dc2d521209179b64c8b57007a9adb39781d
Damyan Pepper <damyanp@gmail.com>
2026-02-01 10:47:42 -0800

    Refactor arranger demo into App::test_arranger method

--- crates/corodaw-widgets/src/main.rs
+++ crates/corodaw-widgets/src/main.rs
@@ -24,6 +24,65 @@ impl App {
         let channels = vec![0, 1];
         Self { channels }
     }
+
+    fn test_arranger(&mut self, ui: &mut Ui) {
+        struct TestArranger<'a>(&'a mut App);
+        impl<'a> ArrangerDataProvider for TestArranger<'a> {
+            fn num_channels(&self) -> usize {
+                self.0.channels.len()
+            }
+
+            fn channel_height(&self, _: usize) -> f32 {
+                100.0
+            }
+
+            fn show_channel(&mut self, index: usize, ui: &mut Ui) {
+                let index = self.0.channels[index];
+
+                let r = ui.available_rect_before_wrap();
+                let p = ui.painter();
+                p.rect_filled(r, 2.0, Color32::DARK_BLUE);
+
+                p.text(
+                    r.center(),
+                    Align2::CENTER_CENTER,
+                    format!("Channel {index}"),
+                    FontId::default(),
+                    ui.style().visuals.text_color(),
+                );
+
+                p.text(
+                    r.left_top(),
+                    Align2::LEFT_TOP,
+                    format!("{},{}", r.left(), r.top()),
+                    FontId::default(),
+                    ui.style().visuals.text_color(),
+                );
+            }
+
+            fn show_strip(&mut self, _: usize, ui: &mut Ui) {
+                let r = ui.available_rect_before_wrap();
+                let p = ui.painter();
+                p.rect_filled(r, 2.0, Color32::DARK_RED);
+            }
+
+            fn on_add_channel(&mut self, index: usize) {
+                self.0.channels.insert(index, self.0.channels.len());
+            }
+
+            fn move_channel(&mut self, index: usize, destination: usize) {
+                let channel = self.0.channels.remove(index);
+                let destination = if destination > index {
+                    destination - 1
+                } else {
+                    destination
+                };
+                self.0.channels.insert(destination, channel);
+            }
+        }
+
+        ArrangerWidget::new("arranger").show(TestArranger(self), ui);
+    }
 }
 
 impl eframe::App for App {
@@ -32,64 +91,9 @@ impl eframe::App for App {
             ui.heading("Corodaw widgets");
 
             CollapsingHeader::new("Arranger")
-                .default_open(true)
+                .default_open(false)
                 .show(ui, |ui| {
-                    struct TestArranger<'a>(&'a mut App);
-                    impl<'a> ArrangerDataProvider for TestArranger<'a> {
-                        fn num_channels(&self) -> usize {
-                            self.0.channels.len()
-                        }
-
-                        fn channel_height(&self, _: usize) -> f32 {
-                            100.0
-                        }
-
-                        fn show_channel(&mut self, index: usize, ui: &mut Ui) {
-                            let index = self.0.channels[index];
-
-                            let r = ui.available_rect_before_wrap();
-                            let p = ui.painter();
-                            p.rect_filled(r, 2.0, Color32::DARK_BLUE);
-
-                            p.text(
-                                r.center(),
-                                Align2::CENTER_CENTER,
-                                format!("Channel {index}"),
-                                FontId::default(),
-                                ui.style().visuals.text_color(),
-                            );
-
-                            p.text(
-                                r.left_top(),
-                                Align2::LEFT_TOP,
-                                format!("{},{}", r.left(), r.top()),
-                                FontId::default(),
-                                ui.style().visuals.text_color(),
-                            );
-                        }
-
-                        fn show_strip(&mut self, _: usize, ui: &mut Ui) {
-                            let r = ui.available_rect_before_wrap();
-                            let p = ui.painter();
-                            p.rect_filled(r, 2.0, Color32::DARK_RED);
-                        }
-
-                        fn on_add_channel(&mut self, index: usize) {
-                            self.0.channels.insert(index, self.0.channels.len());
-                        }
-
-                        fn move_channel(&mut self, index: usize, destination: usize) {
-                            let channel = self.0.channels.remove(index);
-                            let destination = if destination > index {
-                                destination - 1
-                            } else {
-                                destination
-                            };
-                            self.0.channels.insert(destination, channel);
-                        }
-                    }
-
-                    ArrangerWidget::new("arranger").show(TestArranger(self), ui);
+                    self.test_arranger(ui);
                 });
         });
     }
